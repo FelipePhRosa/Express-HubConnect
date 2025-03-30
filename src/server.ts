0eb9349d73861701d41c511b62b6122c93bf0a56
FelipePhRosa <ofcFelipeRosa@gmail.com>
2025-03-29 22:11:05 -0300

    Implemented jwtSecret in .env file.

--- src/server.ts
+++ src/server.ts
@@ -3,31 +3,40 @@ import { routes } from './routes';
 import cors from '@fastify/cors';
 import jwt from '@fastify/jwt';
 import { authenticate } from "./auth";
+import dotenv from 'dotenv';
 
-const server = fastify({ logger: true});
+const server = fastify({ logger: true });
+dotenv.config();
+
+console.log("JWT_SECRET:", process.env.JWT_SECRET); 
 
 const start = async () => {
+    const jwtSecret = process.env.JWT_SECRET;
+
+    if (!jwtSecret) {
+        throw new Error("JWT_SECRET is not defined in .env file");
+    }
 
     server.setErrorHandler((error, request, reply) => {
         reply.code(400).send({ message: error.message });
-    })
+    });
 
     await server.register(cors);
-    
-    // Registrando o plugin JWT
+
+    // Registrando o plugin JWT com a verificação
     await server.register(jwt, {
-        secret: "a63960c60be1671d2f16ec1d8454f005fe7225996716e70a5cc369d901280ea3"
+        secret: jwtSecret
     });
-    
+
     server.decorate("authenticate", authenticate);
 
     await server.register(routes);
-    
-    try{
+
+    try {
         await server.listen({ port: 5173 });
-    }catch(error){
+    } catch (error) {
         process.exit(1);
     }
-}
+};
 
 start();
