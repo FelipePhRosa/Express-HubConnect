f5e2402d543fa15e08d9024a2f8a45e529265c99
FelipePhRosa <ofcFelipeRosa@gmail.com>
2025-03-29 21:26:55 -0300

    Implemented DeleteStore logic.

--- src/services/DeleteStoreService.ts
+++ src/services/DeleteStoreService.ts
@@ -7,18 +7,16 @@ interface DeleteStoreProps{
 
 class DeleteStoreService{
     async execute({ storeId, ownerId }: DeleteStoreProps){
-        // Verificando se os parâmetros estão presentes
+        
         if(!storeId || !ownerId){
             throw new Error("Invalid Request.");
         }
-
         // Verificando se o usuário (owner) existe
         const owner = await prismaClient.user.findFirst({ where: { id: ownerId }});
         if (!owner) {
             throw new Error("User not found!");
         }
-
-        // Buscando a loja com o storeId
+        // Verificando se a loja existe
         const findStore = await prismaClient.store.findFirst({
             where: {
                 id: storeId
@@ -29,18 +27,15 @@ class DeleteStoreService{
             throw new Error("Store not found.");
         }
 
-        // Verificando se o ownerId da loja corresponde ao ownerId que está tentando deletar
         if (findStore.ownerId !== ownerId) {
             throw new Error("You are not authorized to delete this store.");
         }
 
-        // Deletando a loja
         await prismaClient.store.delete({
             where: {    
                 id: findStore.id
             }
         })
-        
         return { message: "Store deleted successfully." }
     }
 }
