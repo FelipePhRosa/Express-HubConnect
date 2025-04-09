29d1cd8d6260ea19536e0a4ff440500550cebb07
FelipePhRosa <ofcFelipeRosa@gmail.com>
2025-04-09 18:45:19 -0300

    Implemented Login Route and Store Creation Route.

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import UserController from "./controllers/userController";
+import StoreController from "./controllers/storeController";
+import { authenticate } from "./services/authMiddleware";
+import AuthController from "./controllers/AuthController";
 
 const router = Router();
 const userController = new UserController();
+const storeController = new StoreController();
+const authController = new AuthController();    
 
 router.get('/', userController.welcome.bind(userController));
+
+router.post('/login', authController.login.bind(authController));
 
 router.post('/user', userController.createUser.bind(userController));
 
@@ -13,5 +20,9 @@ router.get('/usersList', userController.getAllUsers.bind(userController));
 router.get('/userById/:userId', userController.getUserById.bind(userController));
 
 router.delete('/userDel/:userId', userController.deleteUser.bind(userController));
+
+
+//Rotas Protegidas com JWT
+router.post('/store', authenticate, storeController.createStore.bind(storeController));
 
 export default router;
