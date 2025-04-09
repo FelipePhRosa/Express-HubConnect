26c2d95b2afdf08bacfa968ec0b814f7a8e223f6
FelipePhRosa <ofcFelipeRosa@gmail.com>
2025-04-09 18:47:50 -0300

    MiddleWare Check Token validation.

---
+++ src/services/authMiddleware.ts
@@ -0,0 +1,71 @@
+// src/middlewares/authMiddleware.ts
+import { Request, Response, NextFunction } from "express";
+import AuthService from "../services/authService";
+import connection from "../SQL/connection";
+
+interface AuthRequest extends Request {
+  user?: {
+    userId: number;
+    email: string;
+  };
+}
+
+const authService = new AuthService();
+
+// Middleware para verificar se o usuário está autenticado
+export const authenticate = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401).json({ error: "Token ausente ou inválido" });
+      return
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = authService.verifyToken(token);
+
+    const user = await connection("Users")
+      .where({ id_user: decoded.userId })
+      .first();
+
+    if (!user) {
+      res.status(401).json({ error: "Usuário não encontrado" });
+      return
+    }
+
+    req.user = {
+      userId: decoded.userId,
+      email: decoded.email,
+    };
+
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Token inválido ou expirado" });
+    return
+  }
+};
+
+export const isOwner = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await connection("Users")
+      .where({ id_user: req.user?.userId })
+      .first();
+
+    if (!user?.isOwner) {
+      return res.status(403).json({ error: "Acesso negado: precisa ser proprietário" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(500).json({ error: "Erro no servidor ao verificar proprietário" });
+  }
+};
